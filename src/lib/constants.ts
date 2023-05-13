614e4e2d2f4a4c9a429493be23f599eafa8c0057
Cloud-Xero <hiropon201511@gmail.com>
2023-05-14 01:03:43 +0900

    feat: add constants

---
+++ src/lib/constants.ts
@@ -0,0 +1,9 @@
+export const siteMeta = {
+  SITE_TITLE: "My personal website.",
+  SITE_DESCRIPTION: "Welcome to my website!",
+  SITE_URL: "https://******",
+  SITE_TYPE: "website",
+  SITE_LOCALE: "ja_JP",
+  SITE_ICON: "/favicon.svg",
+  SITE_IMG: "/ogp.jpg",
+};
