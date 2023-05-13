ee5740a91707789680f359c0a4e3b9413344d7e4
Cloud-Xero <hiropon201511@gmail.com>
2023-05-14 01:09:11 +0900

    refactor: use double quote

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import { siteMeta } from "./src/lib/constants";
+const { SITE_URL } = siteMeta;
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+  site: SITE_URL,
+  integrations: [mdx(), sitemap()],
 });
