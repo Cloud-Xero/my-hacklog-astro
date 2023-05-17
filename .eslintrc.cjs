97564e0d09f77f1bbc4d276d9db941219e5a8ca8
Cloud-Xero <hiropon201511@gmail.com>
2023-05-17 10:27:56 +0900

    fix: The keyword 'import' is reserved

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -9,20 +9,29 @@ module.exports = {
   overrides: [
     {
       // Define the configuration for `.astro` file.
-      files: ["*.astro"],
+      files: ["**/*.astro"],
       // Allows Astro components to be parsed.
       parser: "astro-eslint-parser",
       // Parse the script in `.astro` as TypeScript by adding the following configuration.
       // It's the setting you need when using TypeScript.
+    },
+    {
+      files: ["**/*.mjs"], // mjsファイルに対する設定
+      parser: "@babel/eslint-parser",
       parserOptions: {
-        parser: "@typescript-eslint/parser",
-        extraFileExtensions: [".astro"],
-      },
-      rules: {
-        // override/add rules settings here, such as:
-        // "astro/no-set-html-directive": "error"
+        requireConfigFile: false,
+        ecmaVersion: 2022,
+        sourceType: "module",
       },
     },
-    // ...
+    {
+      files: ["**/*.ts"],
+      parser: "@typescript-eslint/parser",
+      parserOptions: {
+        project: "./tsconfig.json",
+        ecmaVersion: 2022,
+        sourceType: "module",
+      },
+    },
   ],
 };
